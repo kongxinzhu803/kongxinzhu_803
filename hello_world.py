4be8c1206e87f88e84992892b7a9014e8f2ba0b1
kongxinzhu803 <108617124+kongxinzhu803@users.noreply.github.com>
2024-10-20 19:56:06 +0800

    第一个python
    第一个python，用来最基本的练习

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+print("Input personal information")
+
+name = input("Please enter your name:")
+
+age = input("Please enter you age:")
+
+print("hello ,"+ name +"!")
+print("Today you are "+ age +" years old! realy?")
+
+
